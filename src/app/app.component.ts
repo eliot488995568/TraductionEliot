d95b5e80dd4529f683f6f494fd604446baf75600
Eliot LAUGER <eliot.lauger@hotmail.fr>
2023-05-10 11:35:18 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'traductionEliot';
+  switchLanguage(language: any) {
+    console.log(language);
+    // localStorage.setItem('language', language);
+    // window.location.reload();
+  }
 }
