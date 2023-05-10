88b60f064d2516c4ca0ebe85ccd034b9d3b7bd02
Eliot LAUGER <eliot.lauger@hotmail.fr>
2023-05-10 13:03:21 +0200

    derniere modif

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, NgModule, LOCALE_ID } from '@angular/core';
+import { Component, NgModule, LOCALE_ID, Inject } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,7 +6,7 @@ import { Component, NgModule, LOCALE_ID } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  // constructor(@Inject(LOCALE_ID) private localeId: string) {}
+  constructor(@Inject(LOCALE_ID) private localeId: string) {}
 
   title = 'traductionEliot';
   language: string = 'en';
@@ -14,12 +14,8 @@ export class AppComponent {
   switchLanguage(language: any) {
     this.language = language.value;
     console.log(this.language);
-    if (this.language == 'en') {
-      // si language = en, on affiche le texte en anglais
-    } else if(this.language == 'fr') {
-      // si language = fr, on affiche le texte en français
-    } else if(this.language == 'es') {
-      // si language = es, on affiche le texte en espagnol
+    if (this.language) {
+      this.localeId = this.language;
     } else {
       alert('error');
     }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,11 @@
 import { NgModule, LOCALE_ID } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { registerLocaleData } from '@angular/common';
+import localeFr from '@angular/common/locales/fr';
 
 import { AppComponent } from './app.component';
+
+registerLocaleData(localeFr);
 
 @NgModule({
   declarations: [
